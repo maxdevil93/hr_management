52ebded45d5ba6238ae7d7b7c1d5e2616c3225a8
maxdevil93 <rkddlf3157@gamil.com>
2025-10-02 14:45:21 +0900

    자바 새로 백엔드 추가  디장고 삭제

---
+++ erp-backend/src/test/java/com/company/erp/ErpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
