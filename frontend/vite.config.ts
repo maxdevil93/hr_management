64190f77ead6b75670e951a2655a47f176ac2d52
WannyPark <adgh4728@naver.com>
2025-12-21 16:09:55 +0900

    proxy setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -63,5 +63,12 @@ import autoprefixer from 'autoprefixer';
     server: {
       port: 3000,
       open: true,
+      proxy: {
+        '/api': {
+          target: "http://localhost:8080",
+          changeOrigin: true,
+          secure: false,
+        }
+      }
     },
   });
